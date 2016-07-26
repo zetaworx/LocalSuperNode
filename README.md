5b7d28b9c9dd29a04ffa82483c9ab0f80618778f
Thorben Heinz <thorben@netminion.space>
2016-07-26 08:34:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# LocalSuperNode
+
+Status: Experimental
+License: GPL
+
+This repo contains the configuration to generate an Local Mesh Supernode for Freifunk.
+
+## Configuration
+* eth0: dhcp
+* eth0.vlan12: 172.16.0.1/22; batman-adv, dhcp-server, dns-server
+
+## ToDo
+* use batman-adv backport
+* persistent configuration support and scripts
+
+## History
+
+### Version 0.1.0
+Inital commit
---
+++ config/includes.chroot/root/generate-persistence.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+FILE=/tmp/persistence
+DISK=/dev/disk/by-label/MULTIBOOT
+MNTUSB=/mnt/usb
+MNTPRST=/mnt/persistence
+
+mkdir $MNTUSB
+mkdir $MNTPRST
+
+# create persistence file
+dd if=/dev/null of=$FILE bs=1 count=0 seek=32M
+# format persistence file
+/sbin/mkfs.ext4 -F $FILE
+
+# mount persistence file
+mount -t ext4 /tmp/persistence $MNTPRST
+echo "/etc" > $MNTPRST/persistence.conf
+sync
+umount $MNTPRST
+
+mount -t vfat $DISK $MNTUSB
+cp $FILE $MNTUSB/
+sync
+umount $MNTUSB
+
